fdc2536e686d830c587986e85f59f5a97a1eaaeb
caio17lopes <caio17lopes@gmail.com>
2024-12-01 22:43:40 -0300

    atualização

---
+++ pag83Exercicio.py
@@ -0,0 +1,16 @@
+# exercicios de aplicação de sorted, reverse, sort 
+
+travel = ['japan','italy','coreia','jericoacoara','arraial do cabo']
+print(travel)
+print('My travel list:', sorted(travel))
+print(travel)
+print('My travel list inverted:', sorted(travel, reverse=True))
+print(travel)
+travel.reverse()
+print(travel)
+travel.reverse()
+print(travel)
+travel.sort()
+print(travel)
+travel.sort(reverse=True)
+print(travel)
