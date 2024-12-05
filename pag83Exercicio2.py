8c7632b63ce95f1d293882ccdd30c00061dbdcec
caio17lopes <caio17lopes@gmail.com>
2024-12-05 10:23:25 -0300

    atualização

---
+++ pag83Exercicio2.py
@@ -0,0 +1,8 @@
+# Criando lista de convidados para o jantar
+import time 
+marriage_list = ['dani','rodolfo','mateus','tiago','eduardo','leonardo','amanda','emanoel','valeria','luiz','elisangela','marcelo','sueli']
+print(marriage_list)
+print(sorted(marriage_list))
+print("Vamos ver agora quantos convidados nos temos: ")
+time.sleep(1.0)
+print("nossa lista de convidados tem", len(marriage_list), "pessoas")
---
+++ pag83Exercicio3.py
@@ -0,0 +1,21 @@
+countrys = [
+    "Brasil", "Argentina", "Canadá", "França", "Alemanha", "Itália", "Japão", "Austrália", "China", "India",
+    "México", "Espanha", "Reino Unido", "Estados Unidos", "Rússia", "Egito", "Nigéria", "Indonésia",
+    "Coreia do Sul"
+]
+
+# Convertendo para letras minúsculas
+countrys_lower = [country.lower() for country in countrys]
+
+# Exibindo a lista original em minúsculas
+print("Países em letras minúsculas:", countrys_lower)
+
+# Exibindo a lista em ordem alfabética
+print("Países em ordem alfabética:", sorted(countrys_lower))
+
+# Contagem de paises
+print("A quantidade de paises na lista é de", len(countrys_lower), "no total")
+
+# embaralhando a lista 
+countrys_lower.sort(reverse=True)
+print(countrys_lower)
