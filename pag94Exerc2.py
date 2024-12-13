6b5f0406fdd9fd454d126dfaa7d11da125c1d41b
caio17lopes <caio17lopes@gmail.com>
2024-12-12 21:04:40 -0300

    atualização

---
+++ pag94Exerc2.py
@@ -0,0 +1,5 @@
+passaros = ['calopsita','agapone','ring neck']
+for passaro in passaros:
+    print("O que acha desses animas como bicho de estimação? " + passaro.title())
+    print(" Um cachorro seria ótimo como animal de estimação!")
+print("Sabe o que eles tem em comun?", passaros , ". Todos eles voam :D")
